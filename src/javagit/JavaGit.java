e72dd00b0e6cb7f32693991b726627feca072817
Nao <ktimeabarbara@gmail.com>
2024-10-24 15:56:42 +0200

    feladat megoldasa: hagyomanyos if/else

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -3,10 +3,22 @@ package javagit;
 public class JavaGit {
     
     public static void main(String[] args) {
-    /*ha hideg van, akkor
-        viszek kabátot,
-        egyébként nem
-    */    
+        /*ha hideg van, akkor
+            viszek kabátot,
+            egyébként nem
+        */    
+        final int HIDEG_HATAR = 10;
+        int fok = 7;
+        boolean hidegVan = fok < HIDEG_HATAR;
+        String eredmeny = "";
+        /*hagyományos if:*/
+        if(hidegVan) {
+            eredmeny = "viszek kabátot";
+        } else {
+            eredmeny = "nem viszek kabátot";
+        }
+        System.out.println(eredmeny);
+    
     }
     
 }
