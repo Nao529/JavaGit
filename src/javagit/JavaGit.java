9c30bf6bab620aaf43f3682a0cc44c994198149e
Nao <ktimeabarbara@gmail.com>
2024-10-24 15:49:52 +0200

    komment: feladat leirasa

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -1,9 +1,12 @@
 package javagit;
 
 public class JavaGit {
-
+    
     public static void main(String[] args) {
-        
+    /*ha hideg van, akkor
+        viszek kabátot,
+        egyébként nem
+    */    
     }
     
 }
